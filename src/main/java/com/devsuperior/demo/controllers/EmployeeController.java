9bc74723395b49237cf4521438ff2cafb5c39ee8
Luiz Barros <lck.barros@gmail.com>
2025-11-09 17:17:54 -0300

    feat: implement insert employee endpoint

--- src/main/java/com/devsuperior/demo/controllers/EmployeeController.java
+++ src/main/java/com/devsuperior/demo/controllers/EmployeeController.java
@@ -2,6 +2,8 @@ package com.devsuperior.demo.controllers;
 
 
 
+import java.net.URI;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -10,9 +12,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.demo.dto.EmployeeDTO;
 import com.devsuperior.demo.services.EmployeeService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping(value = "/employees")
@@ -29,4 +35,14 @@ public class EmployeeController {
 			@PageableDefault(sort = "name", direction = Sort.Direction.ASC) Pageable pageable){
 		return ResponseEntity.ok().body(service.findAll(pageable));
 	}
+	
+	@PostMapping
+	public ResponseEntity<EmployeeDTO> insert(@RequestBody EmployeeDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(dto.id()).toUri();
+			return ResponseEntity.created(uri).body(dto);
+	}	
 }
--- src/main/java/com/devsuperior/demo/services/EmployeeService.java
+++ src/main/java/com/devsuperior/demo/services/EmployeeService.java
@@ -6,21 +6,36 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.demo.dto.EmployeeDTO;
+import com.devsuperior.demo.entities.Department;
 import com.devsuperior.demo.entities.Employee;
+import com.devsuperior.demo.repositories.DepartmentRepository;
 import com.devsuperior.demo.repositories.EmployeeRepository;
 
 @Service
 public class EmployeeService {
 	
 	private final EmployeeRepository repository;
+	private final DepartmentRepository departmentRepository;
 
-	public EmployeeService(EmployeeRepository repository) {
+	public EmployeeService(EmployeeRepository repository, DepartmentRepository departmentRepository) {
 		this.repository = repository;
+		this.departmentRepository = departmentRepository;
 	}
 	
 	@Transactional(readOnly = true)
 	public Page<EmployeeDTO> findAll(Pageable pageable){
 		Page<Employee> page = repository.findAll(pageable);
 		return page.map(EmployeeDTO::new);
+	}
+	
+	@Transactional
+	public EmployeeDTO insert(EmployeeDTO dto) {
+		Employee entity = new Employee();
+		entity.setName(dto.name());
+		entity.setEmail(dto.email());
+		Department department = departmentRepository.getReferenceById(dto.departmentId());
+		entity.setDepartment(department);
+		entity = repository.save(entity);
+		return new EmployeeDTO(entity);		
 	}
 }
